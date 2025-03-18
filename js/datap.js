18df7bebf9d5b74e17b712ec9b80a579be04dd7b
Bdullsss <abdaja.9@gmail.com>
2025-03-18 09:14:07 +0700

    update, adding product detail page

---
+++ js/datap.js
@@ -0,0 +1,58 @@
+// Data Produk
+const products = {
+
+// ayam
+  "ayam-1": {
+    name: "Ayam Geprek",
+    image:
+      "https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    price: "Rp 30.000",
+    description:
+      "Ayam goreng geprek dengan sambal pedas yang menggugah selera.",
+  },
+
+//   burger
+  "burger-1": {
+    name: "Burger Keju",
+    image:
+      "https://images.unsplash.com/photo-1605789538467-f715d58e03f9?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "Rp 20.000",
+    description: "Burger dengan keju pilihan yang lumer.",
+  },
+
+//   minuman
+  "minuman-1": {
+    name: "Es Teh",
+    image:
+      "https://images.unsplash.com/photo-1499638673689-79a0b5115d87?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "Rp 7.000",
+    description: "Minuman segar dari susu dan teh pilihan.",
+  },
+
+//   paket
+  "paket-1": {
+    name: "Paket Reguler",
+    image:
+      "https://plus.unsplash.com/premium_photo-1731865337145-c784987cc41a?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "Rp 30.000",
+    description: "Makan anti kenyang.",
+  },
+
+//   deessert
+  "desert-1": {
+    name: "Pudding Keju",
+    image:
+      "https://images.unsplash.com/photo-1620878439728-a8f735a95e3f?q=80&w=2760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "Rp 15.000",
+    description: "Pudding yang lembut dilengkapi dengan keju manis yang lumer.",
+  },
+
+//   side dish
+  "sdish-1": {
+    name: "Kentang Goreng",
+    image:
+      "https://images.unsplash.com/photo-1606755456206-b25206cde27e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "Rp 10.000",
+    description: "Kentang krispi yang digoreng dengan kentang pilihan dan minim minyak.",
+  },
+};
